6e8190e3b1ad09fb79cc42e2829686e1bd8e9666
meta713 <mhdomodomo@gmail.com>
2017-11-05 18:46:23 +0900

    filters調整

--- app/Filters.scala
+++
@@ -1,6 +0,0 @@
-import play.api.http.DefaultHttpFilters
-import play.filters.csrf.CSRFFilter
-import javax.inject.Inject
-
-class Filters @Inject() (csrfFilter: CSRFFilter)
-  extends DefaultHttpFilters(csrfFilter)
